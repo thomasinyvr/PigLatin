dceb3e5695905306c22b4c6a32ced404687a4c9e
thomasinyvr <tomfriesman@gmail.com>
2016-03-11 15:30:11 -0800

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Thomas Friesman on 2016-03-11.
+//  Copyright © 2016 Thomas Friesman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
